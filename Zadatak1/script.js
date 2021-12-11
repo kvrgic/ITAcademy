ba6511f19c8fb8a4e1c904c582f31fcb9d106c49
Aida Kvrgic <kvrgicaida95@gmail.com>
2021-12-11 20:15:00 +0100

    Prvi zadatak

---
+++ Zadatak1/script.js
@@ -0,0 +1,15 @@
+var arr1 = 'Hello World ';
+var arr2 = 'Froma Jamaica';
+
+var arr3 = arr1 + arr2;
+var splitArr3 = arr3.split('');
+
+
+
+for(var i = 0; i < splitArr3.length; i++){
+    if(i % 2 !== 0){
+        splitArr3[i] = `<span class="clr">${splitArr3[i]}</span>`;
+    }
+    
+}
+document.write(splitArr3.join(''));
