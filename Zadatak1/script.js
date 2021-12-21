2b107214d4b2c7eacea4f79769ba562ede315f02
Aida Kvrgic <kvrgicaida95@gmail.com>
2021-12-21 22:23:16 +0100

    Zadatak HTML Box

--- Zadatak1/script.js
+++ Zadatak1/script.js
@@ -7,6 +7,9 @@ var splitArr3 = arr3.split('');
 
 
 for(var i = 0; i < splitArr3.length; i++){
+    if(splitArr3[3] === " "){
+        
+    }
     if(i % 2 !== 0){
         splitArr3[i] = `<span class="clr">${splitArr3[i]}</span>`;
     }
