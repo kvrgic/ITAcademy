121120851ad2e1926648b264006b14cfaa85ac7a
Aida Kvrgic <kvrgicaida95@gmail.com>
2022-01-18 00:29:42 +0100

    Zadatak za zadacu, cetvrti termin

--- JS/Zadatak za zadacu - Cetvrti termin/script.js
+++ JS/Zadatak za zadacu - Cetvrti termin/script.js
@@ -1,15 +1,12 @@
 var arr1 = 'Hello World ';
 var arr2 = 'Froma Jamaica';
 
-var arr3 = arr1 + arr2;
+var arr3 = arr1.concat(arr2);
 var splitArr3 = arr3.split('');
 
 
 
 for(var i = 0; i < splitArr3.length; i++){
-    if(splitArr3[3] === " "){
-        
-    }
     if(i % 2 !== 0){
         splitArr3[i] = `<span class="clr">${splitArr3[i]}</span>`;
     }
