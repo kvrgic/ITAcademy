75bc6af4524c17ee64c354a8850dadefc48d66fb
Aida Kvrgic <kvrgicaida95@gmail.com>
2022-01-24 16:58:14 +0100

    fixxxx životom

--- JS/Validacija da li su unijeti samo brojevi - sedmi termin/script.js
+++ JS/Validacija da li su unijeti samo brojevi - sedmi termin/script.js
@@ -1,10 +1,11 @@
 function validateField(){
     var inputBorder = document.getElementById('jmbg');
-
+    console.log(inputBorder.value);
         if(isNaN(inputBorder.value)){
-            
+            console.log('ima teksta')
             inputBorder.style.border = '2px solid red';
         }else{
+            console.log('nema teksta samo su brojevi')
             inputBorder.style.border = '2px solid black';
         }
 }
